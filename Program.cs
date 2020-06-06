86b7d3a6d5506e088d4d027e01b8e9c20a41d68f
dormrod <david.ormrodmorley@balliol.ox.ac.uk>
2020-06-06 17:01:44 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace book_logger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
