dce04232c2926d54699037b0bc8aaf4df43a5b0c
dormrod <david.ormrodmorley@balliol.ox.ac.uk>
2020-06-08 09:46:21 +0100

    Add menu class for command line interface

--- Program.cs
+++ Program.cs
@@ -4,88 +4,42 @@ namespace BookLogger
 {
 
     class Program
-    {  
+    {
         //App to store user information about books they have read
-        
-		static void Main(string[] args)
-        { 
+
+        static void Main(string[] args)
+        {
 
             //Display welcome message
             Console.WriteLine("Welcome to your Book Logger");
 
-            //Present menu options until user quits
+            //Initialise menus
+            Menu mainMenu = new Menu(new string[] { "Add Book", "Quit" });
+
+            //Run until user quits
             bool quit = false;
             do
             {
-                int option = UserOption();
-
+                //Main menu
+                int option = mainMenu.GetUserOptionCL();
                 switch (option)
                 {
-                    case 0:
-						{ 
-							break;
-						}
                     case 1:
-						{
+                        {
+                            Console.WriteLine("XXX");
                             break;
                         }
-                    default:
+                    case 2:
                         {
                             quit = true;
-                            Console.WriteLine("See you next time");
-							break;
-						}
+                            Console.WriteLine("See you next time!");
+                            break;
+                        }
                 }
-            } while (!quit); 
+            } while (!quit);
 
         }
-
-        static public int UserOption()
-        {
-            //Get user option from menu
-
-            //Display menu until valid option selected
-            bool validOption = false;
-            int option=-1;
-            do
-            {
-                int nOptions = DisplayOptions();
-                try
-                {
-                    option = Convert.ToInt32(Console.ReadLine());
-                    if (option > 0 && option <= nOptions) validOption = true;
-                    else
-                    {
-                        Console.WriteLine("Invalid option");
-                    }
-                }
-                catch
-                {
-                    Console.WriteLine("Invalid option");
-                }
-            } while (!validOption);
-
-            return option;
-		}
-
-        static public int DisplayOptions()
-        {
-            //Display menu options in command line 
-
-            string[] options = { "Add book", "Quit" };
-            int optionNumber = 1;
-
-            Console.WriteLine("What would you like to do?");
-            foreach (string option in options)
-            {
-                Console.WriteLine("{0}) {1}", optionNumber, option);
-                ++optionNumber;
-			}
-
-            return 2;
-		}
-
-
+    
     }
 
 }
---
+++ menu.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace BookLogger
+{
+    public class Menu
+    {
+        //Base menu class
+
+        //Menu options
+        int nOptions; //number of options
+        string[] options = { }; //options to display
+
+        //Constructors
+        public Menu() { }
+        public Menu(string[] menuOptions)
+        {
+            options = menuOptions;
+            nOptions = menuOptions.Length;
+		}
+
+        //Get user menu selection in command line
+        public int GetUserOptionCL()
+        {
+
+            //Display menu until valid option selected
+            bool error;
+            int option = -1;
+            do
+            {
+                DisplayCL();
+                var res = ReadOptionCL();
+                error = res.error;
+                if (error) {
+                    Console.WriteLine("Invalid input");
+				}
+                else option = res.option;
+            } while (error);
+
+            return option;
+		}
+
+		//Display menu options to command line
+        public void DisplayCL() 
+		{
+            int i = 1;
+            foreach (string option in options)
+            {
+                Console.WriteLine("{0}) {1}", i, option);
+                ++i;
+            }
+        }
+
+        //Read user option from command line
+        public (bool error, int option) ReadOptionCL()
+        {
+
+            bool error; //option is invalid or not
+            int option = -1; //placeholder for invalid option
+            try
+            {
+                option = Convert.ToInt32(Console.ReadLine());
+                if (option > 0 && option <= nOptions) error = false;
+                else error = true;
+            }
+            catch
+            {
+                error = true;
+            }
+
+            return (error, option);
+        }
+
+
+
+    }
+}
+
+
+
